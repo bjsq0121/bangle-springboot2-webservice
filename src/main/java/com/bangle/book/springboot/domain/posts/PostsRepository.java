02bf9703d919658cf055d46caf991f0acb9ed865
bjsq0121 <bjsi0121@naver.com>
2021-08-09 23:40:40 +0900

    21/08/09 commit분. Entity Controller Service 등 공부.

---
+++ src/main/java/com/bangle/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.bangle.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
